746491ac12a2225b0520ff38ba2efa550e23edae
st10965 <st10965@iismarconipieralisi.it>
2025-10-13 19:14:32 +0000

    Fine Esperienza

--- src/classicmodels/Queries.sql
+++ src/classicmodels/Queries.sql
@@ -10,3 +10,26 @@ FROM employees
 GROUP BY reportsTo
 ORDER BY numero_dipendenti DESC
 LIMIT 5;
+
+SELECT o.orderNumber
+FROM productLine NATURAL JOIN products NATURAL JOIN orderdetails NATURAL JOIN orders o
+WHERE productLine = 'Classic Cars';
+
+SELECT c.customerNumber, c.customerName, c.phone
+FROM customers c
+WHERE c.customerNumber = (
+    SELECT o.customerNumber
+    FROM orders o
+    GROUP BY o.customerNumber
+    ORDER BY COUNT(*) DESC
+    LIMIT 1
+);
+
+SELECT SUM(p.amount) AS "Somma Totale"
+FROM payments p;
+
+SELECT o.officeCode, COUNT(*) AS numero_dipendenti
+FROM offices o NATURAL JOIN employees e
+GROUP BY o.officeCode
+ORDER BY numero_dipendenti DESC
+LIMIT 1;
