09f377fe558d98e961ed037e1cf8ea25a9b8b519
st10965 <st10965@iismarconipieralisi.it>
2025-11-17 08:35:11 +0000

    QUERY 3

--- src/VERIFICA/query.sql
+++ src/VERIFICA/query.sql
@@ -8,3 +8,9 @@ FROM MateriePrime M NATURAL JOIN Ricette R
 WHERE R.Tipologia = "Burro";
 
 -- Numero di ingredienti utilizzati per ciascuon prodotto
+SELECT COUNT(*) AS Numero, R.Id, P.Nome
+FROM Ricette R NATURAL JOIN Prodotti P
+GROUP BY R.Id;
+
+-- La lista dei dipendenti che non sono responsabili di alcun prodotto
+
