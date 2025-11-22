fbb3559e1392c4690a1d52d41be1d7c10f553169
st10965 <st10965@iismarconipieralisi.it>
2025-11-22 12:05:23 +0000

    Query

--- src/VERIFICA/query.sql
+++ src/VERIFICA/query.sql
@@ -13,15 +13,27 @@ FROM Ricette R NATURAL JOIN Prodotti P
 GROUP BY R.Id;
 
 -- La lista dei dipendenti che non sono responsabili di alcun prodotto
-SELECT
+SELECT *
 FROM Dipendenti d
-WHERE d.Matricola NOT IN { 
-	SELECT
-    FROM
+WHERE d.Matricola NOT IN (
+    SELECT p.Matricola
+    FROM Prodotti p
+    WHERE p.Matricola
+);
 
 -- le migliori 10 materie prime che vengono utilizzata in maggior quantità (intesa come peso totale, non numero di utilizzi)
+SELECT R.Tipologia, SUM(M.PesoUnitario*R.Qta) as Somma_Totale
+FROM MateriePrime M NATURAL JOIN Ricette R
+GROUP BY M.Tipologia
+ORDER BY Somma_Totale DESC
+LIMIT 10
 
 -- il numero di prodotti contenuti in ciascun magazzino, mantenendo soltanto quelli che ne hanno più di 50
+SELECT P.Codice, COUNT(*) AS Numero_Magazzino
+FROM Prodotti P
+GROUP BY P.Codice
+HAVING Numero_Magazzino > 50
+
 
 -- la lista dei prodotti che utilizzano almeno una materia rpima che non è contenuta in alcun magazzino
 
