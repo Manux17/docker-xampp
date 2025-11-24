59de3bb8236ca1007ffeb1293dfe6c5a58771cc6
st10965 <st10965@iismarconipieralisi.it>
2025-11-24 08:31:47 +0000

    Pagina PHP

---
+++ src/TortaAlTesto/risposta.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <title>Risposta</title>
+</head>
+<body>
+    <?php
+        echo "Questa è la pagina risposta";
+    ?>
+</body>
+</html>
