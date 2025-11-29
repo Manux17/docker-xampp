79608bba4e21def4c5529a06b866fec7400787db
st10965 <st10965@iismarconipieralisi.it>
2025-11-29 12:05:14 +0000

    login

---
+++ src/TortaAlTesto/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <title>Risposta</title>
+</head>
+<body>
+    <h1>Questa è la pagina risposta</h1>
+
+    <?php
+        //array super-global (dizionario / array associativi)
+
+        $email = $_POST['email_post']; //email_post è la chiave del nostro array
+        $password = $_POST['password_post'];
+        $nome = $_POST['nome_post'];
+
+        
+        
+        
+    ?>
+</body>
+</html>
