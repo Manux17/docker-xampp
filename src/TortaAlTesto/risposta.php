c6b4b33310996e21640865e7cf7ff74e1f0367c8
st10965 <st10965@iismarconipieralisi.it>
2025-11-29 12:02:50 +0000

    PHP sql

--- src/TortaAlTesto/risposta.php
+++ src/TortaAlTesto/risposta.php
@@ -5,8 +5,23 @@
     <title>Risposta</title>
 </head>
 <body>
+    <h1>Questa è la pagina risposta</h1>
+
     <?php
-        echo "Questa è la pagina risposta";
+        //array super-global (dizionario / array associativi)
+
+        $email = $_POST['email_post']; //email_post è la chiave del nostro array
+        $password = $_POST['password_post'];
+        $nome = $_POST['nome_post'];
+
+        if($email == "ragione@lamia.sempre" && $password == "ciao" && $nome == "Ciao")
+        {
+            echo "LOGIN EFFETTUATA";
+        }
+        else
+        {
+            echo "LOGIN FALLITA";
+        }
     ?>
 </body>
 </html>
