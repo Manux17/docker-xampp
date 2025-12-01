4e8daa53a79e1b11541757ca104f5e1f42cac7c7
st10965 <st10965@iismarconipieralisi.it>
2025-12-01 08:37:39 +0000

    esempio query

--- src/TortaAlTesto/login.php
+++ src/TortaAlTesto/login.php
@@ -2,21 +2,41 @@
 <html lang="it">
 <head>
     <meta charset="UTF-8">
-    <title>Risposta</title>
+    <title>LOGIN</title>
 </head>
 <body>
-    <h1>Questa è la pagina risposta</h1>
+    <h1>Questa è la pagina login</h1>
 
     <?php
+        // passo 1: prendo i dati dalla richiesta http
+        // passo 2: costruisco le query utilizzando i dati dell'utente
+        // passo 3: eseguo le query
+        // passo 4: prendo la risposta della query
+        // passo 5: visualizzo i risultati
+
+        $host = 'db'; 
+        $dbname = 'root_db'; 
+        $user = 'user';
+        $password = 'user';
+        $port = 3306;
+        
+        $connection = new mysqli($host, $user, $password, $dbname, $port);
+        
+        if ($connection->connect_error) {
+            die("Errore di connessione: " . $connection->connect_error);
+        }
+        
+        echo "Connessione al database riuscita con mysqli!";
+        
         //array super-global (dizionario / array associativi)
 
         $email = $_POST['email_post']; //email_post è la chiave del nostro array
         $password = $_POST['password_post'];
         $nome = $_POST['nome_post'];
 
-        
-        
-        
+        $query = "SELECT * FROM utenti WHERE email = '$email' AND password = '$password'"  
+
+        $connection->close();
     ?>
 </body>
 </html>
---
+++ src/hello.php
@@ -0,0 +1,38 @@
+<h1> Hello world! </h1>
+
+<?php
+    //presi dal docker-compose.yml
+    $host = 'db'; 
+    $dbname = 'root_db'; 
+    $user = 'user';
+    $password = 'user';
+    $port = 3306;
+
+    $connection = new mysqli($host, $user, $password, $dbname, $port);
+
+    if ($connection->connect_error) {
+        die("Errore di connessione: " . $connection->connect_error);
+    }
+
+    echo "Connessione al database riuscita con mysqli! <br>";
+
+    $email = "anna.moretti@example.com";
+    $password = "AnnaPass99";
+
+    $query = "SELECT * FROM User WHERE email='$email' AND password='$password'";
+
+    echo $query;
+    echo "<br>";
+
+    $result = $connection->query($query);
+    var_dump($result);
+
+    $row = $result->fetch_assoc();
+    var_dump($row);
+
+
+
+    $connection->close();
+?>
+
+PROVA!
