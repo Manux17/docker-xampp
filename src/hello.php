f1c573afa0129198168dd867418eca87cd51dbc1
st10965 <st10965@iismarconipieralisi.it>
2025-12-02 21:55:06 +0000

    prove while e stampa AT HOME

--- src/hello.php
+++ src/hello.php
@@ -28,8 +28,30 @@
     var_dump($result);
 
     $row = $result->fetch_assoc();
-    var_dump($row);
+    echo "<h1>" . $query . "</h1>";
+    echo "Nome: " . $row['nome'] . "<br>";
+    echo "Email: " . $row['email'] . "<br>";
+    echo "Password: " . $row['password'] . "<br>";
+    echo "<br>";
 
+    //voglio visualizzare i dati dentro la tabella User
+    $query = "SELECT * FROM User";
+    $result = $connection->query($query);
+    echo "<h1>" . $query . "</h1>";
+
+    $row = $result->fetch_assoc(); //senza il while non stampa nulla 
+    echo "Nome: " . $row['nome'] . "<br>";
+    echo "Email: " . $row['email'] . "<br>";
+    echo "Password: " . $row['password'] . "<br>";
+    echo "<br>";
+
+    while ($row = $result->fetch_assoc())  //ho fatto il while perchè abbiamo più righe
+    {
+        echo "Nome: " . $row['nome'] . "<br>";
+        echo "Email: " . $row['email'] . "<br>";
+        echo "Password: " . $row['password'] . "<br>";
+        echo "<br>";
+    }
 
 
     $connection->close();
